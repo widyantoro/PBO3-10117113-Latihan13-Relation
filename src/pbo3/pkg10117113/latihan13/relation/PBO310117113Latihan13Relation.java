4caccf7c6171a1fbd2f803f1376280ff9ba4578b
Eka Widyantoro <ekawidyantoro17@gmail.com>
2018-09-23 18:55:35 +0700

    first commit

---
+++ src/pbo3/pkg10117113/latihan13/relation/PBO310117113Latihan13Relation.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117113.latihan13.relation;
+
+/*
+ * 
+ * @author
+ * Nama     : Eka Widyantoro
+ * Kelas    : IF-3 / PBO3
+ * NIM      : 10117113
+ * Deskripsi Program : Program Relation
+ */
+public class PBO310117113Latihan13Relation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int bill = 10;
+        int bil2 = 20;
+        System.out.println("a == b = " + (bill == bil2));
+        System.out.println("a != b = " + (bill != bil2));
+        System.out.println("a > b = " + (bill > bil2));
+        System.out.println("a < b = " + (bill < bil2));
+        System.out.println("b >= a = " + (bil2 >= bill));
+        System.out.println("b <= a = " + (bil2 <= bill));  
+    }
+    
+}
